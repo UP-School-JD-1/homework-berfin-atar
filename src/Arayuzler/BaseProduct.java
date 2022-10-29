643b6449c4edc5d6c1239f4f6714717cb6585593
Berfiinn <atarberfin@gmail.com>
2022-10-29 18:03:21 +0300

    arayuzler

---
+++ src/Arayuzler/BaseProduct.java
@@ -0,0 +1,32 @@
+package Arayuzler;
+
+
+public abstract class BaseProduct {
+
+    private String name;
+
+    private String description;
+
+    public BaseProduct(String name,  String description) {
+        super();
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+}
