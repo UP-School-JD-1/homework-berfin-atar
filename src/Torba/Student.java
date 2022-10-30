21c72c2f5000fbd5be413d6243cf2e61e8099115
Berfiinn <atarberfin@gmail.com>
2022-10-30 03:46:22 +0300

    torbalar odevi

---
+++ src/Torba/Student.java
@@ -0,0 +1,19 @@
+package Torba;
+
+
+public class Student {
+
+    String name;
+    String surname;
+    int age;
+
+
+
+    public Student(String name, String surname, int age) {
+
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+
+    }
+}
